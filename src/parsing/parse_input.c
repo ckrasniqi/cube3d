69075d3789fc402d11b8da17ae8caba63f80631d
Clirim Krasniqi <ckrasniq@3-F-4.42heilbronn.de>
2025-11-13 19:37:10 +0100

    able to parse textures floor and cieling more or less

--- src/parsing/parse_input.c
+++ src/parsing/parse_input.c
@@ -6,11 +6,11 @@
 /*   By: ckrasniq <ckrasniq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 12:34:49 by ckrasniq          #+#    #+#             */
-/*   Updated: 2025/11/13 18:41:21 by ckrasniq         ###   ########.fr       */
+/*   Updated: 2025/11/13 19:35:14 by ckrasniq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cube3d.h"
+#include "include/cube3d.h"
 
 void	name_check(const char *filename)
 {
@@ -38,90 +38,97 @@ void	validate_map_structure(const char *filename, t_map_data **map_data)
 	(void)map_data;
 }
 
-void	free_lines(char **lines, int line_count, char *last_line)
-{
-	for (int i = 0; i < line_count; i++)
-		free(lines[i]);
-	free(lines);
-	free(last_line);
-	ft_error("Error: Memory allocation failed while reading lines.\n");
-}
 
-void	floor_ceiling_parser(const char *line, t_map_data **map_data)
+int	floor_ceiling_parser(const char *line, t_map_data *map_data)
 {
-	if (!((*map_data)->floor_color) && ft_strncmp(line, "F ", 2) == 0)
+	if (!(map_data->floor_color) && ft_strncmp(line, "F ", 2) == 0)
 	{
 		printf("%s\n", line);
+		return (1);
 	}
-	else if (!((*map_data)->ceiling_color) && ft_strncmp(line, "C ", 2) == 0)
+	else if (!(map_data->ceiling_color) && ft_strncmp(line, "C ", 2) == 0)
 	{
 		printf("%s\n", line);
+		return (1);
+
 	}
+	return (0);
+}
+int	ft_isspace(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' ||
+			c == '\v' || c == '\f' || c == '\r');
 }
 
-void	parse_line(const char *line, t_map_data **map_data)
+char *ft_skip_whitespace(const char *str)
+{
+	while (*str && ft_isspace(*str))
+		str++;
+	return (char *)str;
+}
+
+int	parse_texture_path(const char *line, const char *prefix, char *path)
+{
+	char	*start;
+	char	*end;
+
+	line = ft_skip_whitespace(line);
+	if (ft_strncmp(line, prefix, 2) != 0)
+		return (0);
+	line += 2;
+	line = ft_skip_whitespace(line);
+	if (*line == '\0' || *line == '\n')
+		ft_error("Error: Texture path missing after prefix.\n");
+	start = (char *)line;
+	while (*line != '\0' && *line != '\n' && !ft_isspace(*line))
+		line++;
+	end = (char *)line;
+	*end = '\0';
+	path = malloc(end - start + 1);
+	if (!path)
+	{
+		ft_error("Error: Memory allocation failed for texture path.\n");
+		return (0);
+	}
+	ft_strlcpy(path, start, end - start + 1);
+	path[end - start] = '\0';
+	printf("Parsed %s texture path: %s\n", prefix, path);
+	return (1);
+}
+
+int		parse_line(const char *line, t_map_data *map_data)
 {
 	if (line == NULL || map_data == NULL)
-		return;
-	if (ft_strncmp(line, "NO ", 3) == 0)
-	{
-		if (!((*map_data)->no_path = ft_strdup(line + 3)))
-			ft_error("Error: Memory allocation failed for NO path.\n");
-		printf("%s\n", line);
-	}
-	else if (ft_strncmp(line, "SO ", 3) == 0)
-	{
-		if (!((*map_data)->so_path = ft_strdup(line + 3)))
-			ft_error("Error: Memory allocation failed for SO path.\n");
-	}
-	else if (ft_strncmp(line, "WE ", 3) == 0)
-	{
-		if (!((*map_data)->we_path = ft_strdup(line + 3)))
-			ft_error("Error: Memory allocation failed for WE path.\n");
-	}
-	else if (ft_strncmp(line, "EA ", 3) == 0)
-	{
-		if (!((*map_data)->ea_path = ft_strdup(line + 3)))
-			ft_error("Error: Memory allocation failed for EA path.\n");
-	}
-	else
-		floor_ceiling_parser(line, map_data);
+		return (1);
+	if (parse_texture_path(line, "NO", map_data->no_path) == 1)
+		return (1);
+	if (parse_texture_path(line, "SO", map_data->so_path) == 1)
+		return (1);
+	if (parse_texture_path(line, "WE", map_data->we_path) == 1)
+		return (1);
+	if (parse_texture_path(line, "EA", map_data->ea_path) == 1)
+		return (1);
+	if (floor_ceiling_parser(line, map_data) == 1)
+		return (1);
+	return (0);
 }
 
-void	save_map_data(const char *filename, t_map_data **map_data)
+int	save_map_data(const char *filename, t_map_data *map_data)
 {
 	int		fd;
 	char	*line;
-	char	**lines;
-	int		line_count;
-	// int i = 0;
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		ft_error("Error: Unable to open file.\n");
-	line_count = 0;
-	lines = NULL;
-	while (1)
+	while ((line = get_next_line(fd)) != NULL)
 	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		line_count++;
-		lines = realloc(lines, sizeof(char *) * (line_count + 1));
-		if (!lines)
-			free_lines(lines, line_count - 1, line);
-		lines[line_count - 1] = line;
-		lines[line_count] = NULL;
 		parse_line(line, map_data);
-		// i++;
-		// printf("%d: %s",i,  line);
 	}
+	if (line)
+		free(line);
 	close(fd);
-	(void)map_data;
-
-
-
-
+	return 0;
 }
 
 void	parse_map(const char *filename, t_map_data *map_data)
@@ -133,8 +140,10 @@ void	parse_map(const char *filename, t_map_data *map_data)
 		exit(EXIT_FAILURE);
 	}
 	name_check(filename);
-	save_map_data(filename, &map_data);
+	if (save_map_data(filename, map_data) == 1)
+		// Error already handled in save_map_data
+		return ;
 	validate_sides(filename, &map_data);
 	validate_floor_ceiling(filename, &map_data);
-	printf("Parsing map from file: %s\n", filename);
+	// printf("Parsing map from file: %s\n", filename);
 }
